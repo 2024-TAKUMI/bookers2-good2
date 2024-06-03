b14d1823da04e0f4cb78446bcc14c14c27ab12a2
2024-TAKUMI <ec2-user@ip-172-31-44-129.ap-northeast-1.compute.internal>
2024-06-03 08:55:24 +0000

    [27/27]bookers2-debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  #before_action :ensure_correct_user, only: [:update]
-  before_action :authenticate_user!
+
+  before_action :is_matching_login_user, only: [:edit, :destroy]
 
   def show
     @user = User.find(params[:id])
@@ -30,13 +30,14 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :email, :title, :body, :introduction, :image, :profile_image)
   end
 
-  def ensure_correct_user
-    @user = User.find(params[:id])
-    unless @user == current_user
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
       redirect_to user_path(current_user)
     end
   end
+
 end
